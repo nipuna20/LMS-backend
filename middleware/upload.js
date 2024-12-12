70d19cf53e1bc9dfe2be85c80b57e689c93cc647
nipuna20 <nipunau739@gmail.com>
2024-12-12 22:30:30 +0530

    ee

--- middleware/upload.js
+++ middleware/upload.js
@@ -10,38 +10,37 @@ const destination = process.env.DESTINATION_UPLOAD || './uploads';
 
 // Ensure the upload directory exists
 if (!fs.existsSync(destination)) {
-  fs.mkdirSync(destination, { recursive: true });
+    fs.mkdirSync(destination, { recursive: true });
 }
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, destination);
-  },
-  filename: (req, file, cb) => {
-    const randomNumber = Math.floor(Math.random() * 1000);
-    cb(
-      null,
-      `${file.fieldname}_${Date.now()}_${randomNumber}${path.extname(
-        file.originalname
-      )}`
-    );
-  },
+    destination: (req, file, cb) => {
+        cb(null, destination);
+    },
+    filename: (req, file, cb) => {
+        const randomNumber = Math.floor(Math.random() * 1000);
+        cb(
+            null,
+            `${file.fieldname}_${Date.now()}_${randomNumber}${path.extname(
+                file.originalname
+            )}`
+        );
+    },
 });
 
 const fileFilter = (req, file, cb) => {
-  // Updated to allow PDF files
-  const allowType = ['image/jpeg', 'image/png', 'application/pdf'];
-  if (allowType.includes(file.mimetype)) {
-    cb(null, true); // Accept
-  } else {
-    cb(new Error('Only JPG, PNG, and PDF files are allowed!'), false);
-  }
+    const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only JPG, PNG, and PDF files are allowed!'), false);
+    }
 };
 
 const upload = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: maxSize },
+    storage,
+    fileFilter,
+    limits: { fileSize: maxSize },
 });
 
 module.exports = upload;
