9040d2109982b9f37c0c4f12d919e08c2cde7add
nipuna20 <nipunau739@gmail.com>
2024-12-12 17:20:32 +0530

    pp

--- model/certificate.js
+++ model/certificate.js
@@ -1,30 +1,30 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const certificateSchema = new mongoose.Schema(
-//     {
-//         filename: {
-//             type: String,
-//             required: true,
-//         },
-//         filepath: {
-//             type: String,
-//             required: true,
-//         },
-//         uploadedBy: {
-//             type: String, // For example, user ID or email
-//             required: true,
-//         },
-//         uploadedAt: {
-//             type: Date,
-//             default: Date.now,
-//         },
-//         qrCode: {
-//             type: String, // Base64 QR code or a URL pointing to the QR code image
-//         },
-//     },
-//     {
-//         timestamps: true, // Automatically manage createdAt and updatedAt fields
-//     }
-// );
+const certificateSchema = new mongoose.Schema(
+    {
+        filename: {
+            type: String,
+            required: true,
+        },
+        filepath: {
+            type: String,
+            required: true,
+        },
+        uploadedBy: {
+            type: String, // For example, user ID or email
+            required: true,
+        },
+        uploadedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        qrCode: {
+            type: String, // Base64 QR code or a URL pointing to the QR code image
+        },
+    },
+    {
+        timestamps: true, // Automatically manage createdAt and updatedAt fields
+    }
+);
 
-// module.exports = mongoose.model("Certificate", certificateSchema);
+module.exports = mongoose.model("Certificate", certificateSchema);
